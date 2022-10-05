caf1dda261aa74f5f18b6b7fae6141f6650c25ed
Wagão - ( Wagner Goulart ) <wagner.goulart@neon.com.br>
2022-10-05 00:07:55 -0300

    Initial Commit

---
+++ uber/uberApp.swift
@@ -0,0 +1,17 @@
+//
+//  uberApp.swift
+//  uber
+//
+//  Created by Wagner Barboza Goulart on 05/10/22.
+//
+
+import SwiftUI
+
+@main
+struct uberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
